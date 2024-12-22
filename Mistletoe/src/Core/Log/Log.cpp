a5a4cad5a8afc93a3e7961b2cc7b7202b2738e59
Leyan Cui <leyancui@foxmail.com>
2024-12-22 21:16:57 +0800

    fix: update .gitignore to avoid ignoring Log folder

---
+++ Mistletoe/src/Core/Log/Log.cpp
@@ -0,0 +1,26 @@
+#include "mstpch.h"
+#include "Log.h"
+
+namespace Mistletoe
+{
+	std::shared_ptr<spdlog::logger> Log::coreLogger;
+	std::shared_ptr<spdlog::logger> Log::clientLogger;
+
+	Log::Log() {}
+	Log::~Log() {}
+
+	void Log::Init()
+	{
+		spdlog::set_pattern("%^[%T] %n: %v%$");
+		coreLogger = spdlog::stdout_color_mt("Mistletoe");
+		coreLogger->set_level(spdlog::level::trace);
+		clientLogger = spdlog::stdout_color_mt("Application");
+		clientLogger->set_level(spdlog::level::trace);
+	}
+
+	void Log::Shutdown()
+	{
+		coreLogger.reset();
+		clientLogger.reset();
+	}
+}
